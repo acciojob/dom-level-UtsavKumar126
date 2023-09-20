62f2c3a3e26ba374a849254451a65f8868219d27
UtsavKumar126 <117590564+UtsavKumar126@users.noreply.github.com>
2023-09-20 13:52:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+function getDOMLevel(elementId) {
+    const element = document.getElementById(elementId);
+    if (!element) {
+        alert("Element with id " + elementId + " not found.");
+        return;
+    }
+
+    let level = 0;
+    let currentNode = element;
+
+    while (currentNode !== document.documentElement) {
+        currentNode = currentNode.parentElement;
+        level++;
+    }
+
+    // Include the root HTML element in the level count
+    level++;
+
+    alert("The level of the element is: " + level);
+}
+
+// Call the function with the desired element ID
+getDOMLevel("level");
